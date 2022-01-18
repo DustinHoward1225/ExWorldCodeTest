627ad5e254078a3340be19de787e2c632e122b48
DustinHoward1225 <dustinhoward1225@gmail.com>
2022-01-18 00:06:48 -0500

    Updated .gitignore to ignore the .vs folder

--- CodeTestProject/ProgrammingTestProj/Intermediate/Build/Win64/ProgrammingTestProjEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30133
-#include "C:/Users/dusti/Documents/Github/ExWorldTest/ExWorldCodeTest/CodeTestProject/ProgrammingTestProj/Intermediate/Build/Win64/ProgrammingTestProjEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
